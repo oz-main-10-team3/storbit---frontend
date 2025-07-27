c8060794d38b4610833dc77aa99652e9c841bc32
yongarframe <yongar2002@gmail.com>
2025-07-27 20:23:56 +0900

    🎨 Design: 메인 색상 및 주요 색상 변수 정의, 메인 폰트 설정 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [test, setTeste] = useState(10)
   return (
     <>
-      <div>{test}123123123</div>
-      <button onClick={() => setTeste((prev) => prev + 1)}>버튼</button>
+      <div className="text-primary hover:bg-primary-hover">
+        메인 색상 및 호버 색상
+      </div>
+      <div className="text-text">기본 텍스트 색상</div>
+      <div className="text-text2">보조 텍스트 색상</div>
+      <div className="text-text3 bg-black">흰색 텍스트 색상</div>
+      <div className="text-alertText">경고 텍스트 색상</div>
+      <div className="text-text3 bg-bg">어두운 배경 색상</div>
+      <div className="text-disabled-text bg-disabled-fill">
+        비활성화된 텍스트 배경색상
+      </div>
     </>
   )
 }
