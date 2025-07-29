b7dc17b99352f87bf5b466742dd901efba835f47
yongarframe <yongar2002@gmail.com>
2025-07-29 18:28:49 +0900

    ✨ feat: footer 에 있는 페이지 추가 및 라우팅 설정

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom'
+
 export default function Footer() {
   return (
     <footer className="w-full bg-[#45484C] h-[320px] py-[64px] px-[260px]">
       <div className="flex w-[400px] h-[20px] items-center justify-center text-text3 text-[16px] gap-[8px]">
-        <div>이용약관</div>
+        <Link className="cursor-pointer" to="/terms">
+          이용약관
+        </Link>
         <div>|</div>
-        <div>개인정보처리방침</div>
+        <Link className="cursor-pointer" to="/privacy">
+          개인정보처리방침
+        </Link>
         <div>|</div>
-        <div>문의하기</div>
+        <Link className="cursor-pointer" to="contact">
+          문의하기
+        </Link>
         <div>|</div>
         <div>© 2025 storbit</div>
       </div>
---
+++ src/pages/contact/ContactPage.tsx
@@ -0,0 +1,3 @@
+export default function ContactPage() {
+  return <div>문의하기</div>
+}
