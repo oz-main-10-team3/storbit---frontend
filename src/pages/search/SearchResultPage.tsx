b805867b8ebe1a7ba2cc02edb8ca4e69810cc386
yongarframe <yongar2002@gmail.com>
2025-07-29 13:57:55 +0900

    ✨ feat: 상단 navbar UI 작성

--- src/pages/search/SearchResultPage.tsx
+++ src/pages/search/SearchResultPage.tsx
@@ -1,3 +1,11 @@
+import { useParams } from 'react-router-dom'
+
 export default function SearchResultPage() {
-  return <div className="text-2xl">검색 결과 페이지</div>
+  const { query } = useParams()
+  return (
+    <>
+      <div className="text-2xl text-alertText">{query} </div>
+      <div className="text-2xl">검색 결과 페이지</div>
+    </>
+  )
 }
