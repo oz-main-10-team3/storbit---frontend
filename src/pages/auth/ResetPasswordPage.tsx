4714c99e7ad2e6db756bf3c6ef5a960e1c81c59b
yongarframe <yongar2002@gmail.com>
2025-07-30 23:50:43 +0900

    ✨ feat: 비밀번호 재설정 페이지 UI 작성

--- src/pages/auth/ResetPasswordPage.tsx
+++ src/pages/auth/ResetPasswordPage.tsx
@@ -1,15 +1,55 @@
-import { Link } from 'react-router-dom'
+import InputField from '@/common/InputField'
+import { useState } from 'react'
+import CommonButton from '@/common/CommonButton'
+import { useNavigate } from 'react-router-dom'
 
 export default function ResetPasswordPage() {
+  const [newPassword, setNewPassword] = useState('')
+  const [checkNewPassword, setCheckNewPassword] = useState('')
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/auth/reset-password/success')
+  }
   return (
-    <>
-      <div className="text-2xl">비밀번호 재설정 페이지</div>
-      <Link
-        className="bg-emerald-300 rounded-[5px] w-[150px] h-[30px] flex items-center justify-center"
-        to="/auth/reset-password/success"
-      >
-        비밀번호 재설정 완료
-      </Link>
-    </>
+    <div className="flex flex-col items-center justify-center bg-white h-[856px]">
+      <div className="flex flex-col items-center justify-center gap-[64px]">
+        <div className="flex flex-col items-center justify-center gap-[24px]">
+          <div className="text-[32px] font-semibold">비밀번호 찾기</div>
+          <div className="flex flex-col items-center justify-center text-[16px] text-disabled-text">
+            <div>test123@storbit.com의 비밀번호를 재설정 합니다</div>
+            <div>
+              영문, 숫자, 특수문자 포함하여 공백없이 8~20자 입력 해주세요
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-[40px]">
+          <div className="flex flex-col w-[400px] pace-y-3 gap-[12px]">
+            <InputField
+              className="w-full h-12 placeholder:text-text4"
+              placeholder="새 비밀번호"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              type="number"
+              // error={error}
+              // success={success}
+            />
+            <InputField
+              className="w-full h-12 placeholder:text-text4"
+              placeholder="새 비밀번호 확인"
+              value={checkNewPassword}
+              onChange={(e) => setCheckNewPassword(e.target.value)}
+              type="number"
+              // error={error}
+              // success={success}
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-[24px] w-full">
+            <CommonButton className="text-[15px]" onClick={handleClick}>
+              저장
+            </CommonButton>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
